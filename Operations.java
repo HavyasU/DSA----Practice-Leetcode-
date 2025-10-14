d84a8b2ca6b8bca0ab1e9b0abad43d7df78e11e0
havyasu <havyasu2005@gmail.com>
2025-10-14 20:59:20 +0530

    Java Practice Problems

---
+++ Operations.java
@@ -0,0 +1,18 @@
+public class Operations {
+    public static void main(String[] args) {
+
+        int num1 = 15;
+        int num2 = 15;
+
+        // boolean result = num1 >= num2 && num1 > 0;
+
+        if (num1 > num2) {
+            System.out.println("Num1 is Greater! ");
+        } else if (num2 > num1) {
+            System.out.println("Num2 is Greater! ");
+        } else {
+            System.out.println("BOth Are Equal ");
+        }
+
+    }
+}
