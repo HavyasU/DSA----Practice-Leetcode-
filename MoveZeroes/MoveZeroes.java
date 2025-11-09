e9627a21889416e29dc3f5cd1935b1dfed0228f4
havyasu <havyasu2005@gmail.com>
2025-11-09 21:58:46 +0530

    Leetcode Solution

---
+++ MoveZeroes/MoveZeroes.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int temp;
+        for (int i = 0, k = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                temp = nums[k];
+                nums[k] = nums[i];
+                nums[i] = temp;
+                k++;
+            }
+        }
+    }
+}
