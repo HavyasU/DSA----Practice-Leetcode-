a75e06b8a2405e50a5c0252c5d7514d95f4ed7f1
havyasu <havyasu2005@gmail.com>
2025-11-20 23:48:58 +0530

    Leetcode Solution

---
+++ 747.LargestNumberAtLeastTwiceofOthers/747.LargestNumberAtLeastTwiceofOthers.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int dominantIndex(int[] nums) {
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > nums[max])
+                max = i;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != nums[max] && nums[max] < nums[i] * 2) {
+                System.out.println(nums[i]);
+                return -1;
+            }
+        }
+
+        return max;
+
+    }
+}
