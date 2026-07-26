89b5e13e570104b483756270fabbd91bce6ec20e
Havyas U <113080389+HavyasU@users.noreply.github.com>
2026-07-26 20:38:24 +0530

    Time: 1 ms (96.64%), Space: 42.4 MB (73.84%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,16 +1,11 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n<=0){
-            return false;
+        if(n==1) return true;
+        for(long i=2;i<=n;i*=2){
+            if(i==n) return true;
         }
-        while(n>1){
-            if(n%2==1){
-                return false;
-            }
-            n=n/2;  
-        }
-        return true;
-
-        
+        return false;
     }
 }
+
+
