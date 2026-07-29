de0bf707d07e33aa3b721e0281b7c3d74815f87b
Havyas U <113080389+HavyasU@users.noreply.github.com>
2026-07-29 11:40:24 +0530

    Time: 11 ms (6.06%), Space: 215.9 MB (98.21%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -12,27 +12,28 @@ class Solution {
     public ListNode mergeNodes(ListNode head) {
        ListNode temp = head.next;
 
-       ListNode res = null;
-       ListNode tail = null;
+       ArrayList<Integer> lst = new ArrayList<>();
+
+       ListNode res =new ListNode(0);
+       ListNode tail = res;
        
        int sum =0;
        while(temp!=null){
         if(temp.val != 0){
             sum+=temp.val;
         }else{
-            ListNode dummy = new ListNode(sum);
-            dummy.next = null;
-            if(tail == null){
-                tail = dummy;
-                res = tail;
-            }else{
-                tail.next = dummy;
-                tail = tail.next;
-            }
-            sum=0;
+            lst.add(sum);
+            sum = 0;
         }
         temp = temp.next;
        }
+
+
+       for(int ele : lst){
+            tail.next = new ListNode(ele);
+            tail = tail.next;
+       }
+       res = res.next;
        return res; 
     }
 }
