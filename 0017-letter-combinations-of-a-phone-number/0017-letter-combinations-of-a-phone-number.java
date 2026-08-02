4008a5c72a9674ff8c27cb45c8dd902abddafcff
Havyas U <113080389+HavyasU@users.noreply.github.com>
2026-08-02 22:54:31 +0530

    Time: 0 ms (100%), Space: 43.4 MB (77.87%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,38 +1,26 @@
 class Solution {
     HashMap<Integer, String> map = new HashMap<>();
-
-   
-
-    int n;
-    String digits;
-    ArrayList<String> lst = new ArrayList<>();
-
-    void DFS(int index, StringBuilder sb){
-        if(index == n){
-            lst.add(sb.toString());
+    String[] keys = {
+        "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+    };
+    void DFS(String digits, int index, StringBuilder sb, ArrayList<String> result){
+        if(index == digits.length()){
+            result.add(sb.toString());
             return;
         }
-        String str = map.get((digits.charAt(index)-'0'));
+        String str = keys[digits.charAt(index)-'0'];
         for(char c : str.toCharArray()){
             sb.append(c);
-            DFS(index+1,sb);
+            DFS(digits, index+1, sb, result);
             sb.deleteCharAt(sb.length()-1);
         }
     }
 
     public List<String> letterCombinations(String digits) {
-        this.digits = digits;
-        this.n  = digits.length();
-        map.put(2,"abc");
-        map.put(3,"def");
-        map.put(4,"ghi");
-        map.put(5,"jkl");
-        map.put(6,"mno");
-        map.put(7,"pqrs");
-        map.put(8,"tuv");
-        map.put(9,"wxyz");
+        ArrayList<String> result = new ArrayList<>();
+        if(digits.isEmpty()) return result;        
 
-        DFS(0, new StringBuilder());
-        return lst;
+        DFS(digits, 0, new StringBuilder(), result);
+        return result;
     }
 }
